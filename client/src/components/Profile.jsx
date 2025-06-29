06b02be2a921f6908f46d63dbf9b530d9acd68fa
Prem Roshan <premroshan209@gmail.com>
2025-06-29 19:00:45 +0530

    Added functionality to upload and delete files

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { deleteFile, uploadFile } from "../utils/mediaHandlers";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [file, setFile] = useState("");
+  const [ImageUrl, setImageUrl] = useState("");
+  useEffect(() => {
+    console.log(file);
+    console.log(ImageUrl);
+    console.log(new Date().getTime().toString());
+  }, [file, ImageUrl]);
+
+  const fileUpload = (e) => {
+    e.preventDefault();
+    file && uploadFile(e, file, "enlightasp@gmail.com", setImageUrl);
+  };
+
+  // This component allows users to upload and delete files, displaying the uploaded file's URL.
+  // It uses the `uploadFile` and `deleteFile` utility functions to handle file operations
+  // and maintains the state of the uploaded file and its URL.
+  return (
+    <form>
+      <label htmlFor="image">
+        <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        <button
+          onClick={(e) => {
+            fileUpload(e);
+          }}
+        >
+          submit
+        </button>
+      </label>
+      <h1>{ImageUrl}</h1>
+      <button
+        onClick={(e) => {
+          deleteFile(e, ImageUrl);
+        }}
+      >
+        delete
+      </button>
+    </form>
+  );
 };
 
 export default Profile;
